3b23f87f1ec98a0932a1e53806f6f5b5cf5270c5
dnr14 <dnr14@naver.com>
2021-12-02 06:52:15 +0900

    feat : app.js update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>젠킨스로 ci/cd를 해보자꾸나</p>
         <a
           className="App-link"
           href="https://reactjs.org"
